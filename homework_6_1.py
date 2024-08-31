19a4ed2b951153cc5e4c04f8cc41e8d1e76c0488
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-08-31 18:10:32 +0500

    Update homework_6_1.py

--- homework_6_1.py
+++ homework_6_1.py
@@ -4,25 +4,19 @@ class Animal:
         self.alive = True
         self.fed = False
 
-class Mammal(Animal):
-
     def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
+
+class Mammal(Animal):
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
-
+    pass
 
 class Plant:
     edible = False
